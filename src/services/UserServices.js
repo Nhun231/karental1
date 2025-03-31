8dfc837c31ec1f690cdeb46db84b5b7b66919442
ANHBHN2 <anhbhn2@fpt.com>
2025-03-31 16:44:19 +0700

    deploy 2.2

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = BASE;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const registerUser = async (userData) => {
   try {
     const response = await fetch(`${BASE_URL}/user/register`, {
