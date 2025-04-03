44a9ef49a255fd0177e6ff7c25636fc9ae1432c4
Bui Minh Chien <chien181004@gmail.com>
2025-04-03 10:56:36 +0700

    fix modal and add "/user" to change password url

--- src/components/common/ConfirmationDialog.js
+++ src/components/common/ConfirmationDialog.js
@@ -1,35 +1,56 @@
 import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  Button,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+    Button, IconButton, DialogContentText, Stack,
 } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
 /**
- * 
+ *
  * Confirmation Dialog Component
  * Provide Dialog UI
  */
 export default function ConfirmationDialog({
-  open,
-  onClose,
-  onConfirm,
-  title,
-  content,
-  loading
-}) {
-  return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>{title}</DialogTitle>
-      <DialogContent>{content}</DialogContent>
-      <DialogActions>
-        <Button onClick={onClose} color="secondary">
-          Cancel
-        </Button>
-        <Button onClick={onConfirm} color="primary" variant="contained" disabled={loading}>
-          {loading ? "Processing..." : "Confirm"}
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
+                                               open,
+                                               onClose,
+                                               onConfirm,
+                                               title,
+                                               content,
+                                               loading
+                                           }) {
+    return (
+        <Dialog open={open} onClose={onClose} aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                closeAfterTransition={false}>
+            <DialogTitle id="alert-dialog-title" sx={{
+                padding: "16px 24px 0px !important",
+                fontWeight: "bold",
+                minWidth: "300px",
+            }}>{title}</DialogTitle>
+            <IconButton aria-label="close" onClick={onClose}
+                        sx={(theme) => ({position: "absolute", right: 8, top: 8, color: theme.palette.grey[500],})}>
+                <CloseIcon/>
+            </IconButton>
+            <DialogContent sx={{padding: "16px 24px !important"}}>
+                <DialogContentText id="alert-dialog-description">{content}</DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Stack direction="row" spacing={2} sx={{
+                    width: "100%",
+                    justifyContent: "center",
+                    alignItems: "flex-start",
+                    marginBottom: 1,
+                }}>
+                    <Button variant="contained" onClick={onClose} color="secondary" sx={{width: "150px"}}>
+                        Cancel
+                    </Button>
+                    <Button onClick={onConfirm} color="primary" variant="contained" disabled={loading} sx={{width: "150px"}}>
+                        {loading ? "Processing..." : "Confirm"}
+                    </Button>
+                </Stack>
+            </DialogActions>
+        </Dialog>
+    );
 }
