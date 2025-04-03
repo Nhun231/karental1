2bbde8712676b1ce7fe9993db6b76de334cae968
Bui Minh Chien <chien181004@gmail.com>
2025-04-03 18:17:55 +0700

    fix confirm dialog

--- src/components/common/ConfirmationDialog.js
+++ src/components/common/ConfirmationDialog.js
@@ -3,33 +3,54 @@ import {
   DialogTitle,
   DialogContent,
   DialogActions,
-  Button,
+  Button, IconButton, DialogContentText, Stack,
 } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
 /**
- * 
+ *
  * Confirmation Dialog Component
  * Provide Dialog UI
  */
 export default function ConfirmationDialog({
-  open,
-  onClose,
-  onConfirm,
-  title,
-  content,
-  loading
-}) {
+                                             open,
+                                             onClose,
+                                             onConfirm,
+                                             title,
+                                             content,
+                                             loading
+                                           }) {
   return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>{title}</DialogTitle>
-      <DialogContent>{content}</DialogContent>
-      <DialogActions>
-        <Button onClick={onClose} color="secondary" variant="contained" sx={{ bgcolor: "#aaabad" }}>
-          Cancel
-        </Button>
-        <Button onClick={onConfirm} variant="contained" disabled={loading} sx={{ bgcolor: "#05ce80" }}>
-          {loading ? "Processing..." : "Confirm"}
-        </Button>
-      </DialogActions>
-    </Dialog>
+      <Dialog open={open} onClose={onClose} aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+              closeAfterTransition={false}>
+        <DialogTitle id="alert-dialog-title" sx={{
+          padding: "16px 24px 0px !important",
+          fontWeight: "bold",
+          minWidth: "300px",
+        }}>{title}</DialogTitle>
+        <IconButton aria-label="close" onClick={onClose}
+                    sx={(theme) => ({position: "absolute", right: 8, top: 8, color: theme.palette.grey[500],})}>
+          <CloseIcon/>
+        </IconButton>
+        <DialogContent sx={{padding: "16px 24px !important"}}>
+          <DialogContentText id="alert-dialog-description">{content}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Stack direction="row" spacing={2} sx={{
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "flex-start",
+            marginBottom: 1,
+          }}>
+            <Button variant="contained" onClick={onClose} color="inherit" sx={{width: "150px"}}>
+              Cancel
+            </Button>
+            <Button onClick={onConfirm} variant="contained" disabled={loading} sx={{width: "150px", backgroundColor: "#05ce80"}}>
+              {loading ? "Processing..." : "Confirm"}
+            </Button>
+          </Stack>
+        </DialogActions>
+      </Dialog>
   );
 }
